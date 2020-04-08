987144ece949dc8f21aed07986a68beb2aeb0da5
Jonathan Arnaldo <jonarnaldo@jonathans-air-2.lan>
2020-04-08 12:02:55 -0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   console.log(`Server started at port ${process.env.PORT}`)
   await app.listen(process.env.PORT);
 }
